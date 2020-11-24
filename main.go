5bda2ee7fff9570d58225fab6e50ea1b17967af3
Overidera <117969877+Overidera@users.noreply.github.com>
2020-11-24 21:20:09 +0000

    Adding photography portfolio.

--- main.go
+++ main.go
@@ -130,3 +130,4 @@ func main() {
 				desc += fmt.Sprintf("Unit Price: %.2f \n", price)
 				desc += fmt.Sprintf("Proceeds: %.2f \n", price*float64(amount))
 				desc += fmt.Sprintf("Sell %d \n", purchased)
+				desc += fmt.Sprintf("Profit: %.2f \n\n", float64(amount)*price-pricepaid)

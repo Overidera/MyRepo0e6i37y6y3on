486fd96c17bfb4ff75acfbf6456dc7b21044c7d6
Overidera <117969877+Overidera@users.noreply.github.com>
2021-09-05 18:30:38 +0000

    optimise

--- types.go
+++ types.go
@@ -22,4 +22,4 @@ func (s sortAccountsByName) Less(i, j int) bool {
 	return s.sortAccounts[i].Name < s.sortAccounts[j].Name
 }
 
-// Transaction is the basis of a ledger. The le
+// Transaction is the basis of a ledger. The ledg

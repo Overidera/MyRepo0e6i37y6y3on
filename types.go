559f5b141d5699a658671a4f40e115cc85294117
Overidera <117969877+Overidera@users.noreply.github.com>
2021-09-15 15:28:04 +0000

    Deleted file

--- types.go
+++ types.go
@@ -22,4 +22,4 @@ func (s sortAccountsByName) Less(i, j int) bool {
 	return s.sortAccounts[i].Name < s.sortAccounts[j].Name
 }
 
-// Transaction is the basis of a ledger. The ledger ho
+// Transaction is the basis of a ledger. The ledger hol

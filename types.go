042445645bae6fead162a2d4161d0ca0c0c002bd
Overidera <117969877+Overidera@users.noreply.github.com>
2021-10-15 12:06:42 +0000

    added features

--- types.go
+++ types.go
@@ -22,4 +22,4 @@ func (s sortAccountsByName) Less(i, j int) bool {
 	return s.sortAccounts[i].Name < s.sortAccounts[j].Name
 }
 
-// Transaction is the basis of a ledger. The ledger holds a list of transac
+// Transaction is the basis of a ledger. The ledger holds a list of transact
